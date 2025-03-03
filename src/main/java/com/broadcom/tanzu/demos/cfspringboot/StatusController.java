fc10f31e9ef17e290ac37b169952297ff1f67f0d
Alexandre Roman <alexandre.roman@broadcom.com>
2025-03-03 18:00:12 +0100

    Add an emoji icon in front of the status code

--- src/main/java/com/broadcom/tanzu/demos/cfspringboot/StatusController.java
+++ src/main/java/com/broadcom/tanzu/demos/cfspringboot/StatusController.java
@@ -2,7 +2,7 @@ package com.broadcom.tanzu.demos.cfspringboot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +16,14 @@ class StatusController {
     @GetMapping(value = "/status/{code}", produces = MediaType.TEXT_PLAIN_VALUE)
     ResponseEntity<String> status(@PathVariable int code) {
         logger.atInfo().log("Generating HTTP status code: {}", code);
-        return ResponseEntity.status(code).body("Status code: %s".formatted(HttpStatusCode.valueOf(code)));
+        final var status = HttpStatus.valueOf(code);
+        final var emoji = switch (status.series()) {
+            case INFORMATIONAL -> "\uD83E\uDD1D";
+            case SUCCESSFUL -> "\uD83D\uDE03";
+            case REDIRECTION -> "\uD83D\uDC49";
+            case CLIENT_ERROR -> "\uD83D\uDCA9";
+            case SERVER_ERROR -> "\uD83D\uDD25";
+        };
+        return ResponseEntity.status(code).body("%s Status code: %s".formatted(emoji, status));
     }
 }
